5295945e0a78daa20ad9e37d96877ab946f79017
Kjrinverse <emailziyang@gmail.com>
2025-05-23 15:50:50 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cloud Accounting Application</h1>
+        <p>Welcome to your cloud accounting system</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
